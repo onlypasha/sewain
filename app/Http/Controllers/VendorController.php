3ec6ca8be9b88102bc20e7bf23cc2e2aafcd6c84
onlypasha <onlypasha7@outlook.co.id>
2026-08-14 11:02:58 +0700

    adding custom request for Vendor

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -2,18 +2,36 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Vendor;
-use Illuminate\Http\Request;
+use App\Http\Requests\CreateVendorRequest;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class VendorController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         // get senmua data vendor
         $vendors = User::where('role', 'vendor')->get();
         return view('vendor.index', compact('vendors'));
     }
 
-    public function store(){
-        return view('vendor.create');
+    public function store(CreateVendorRequest $request)
+    {
+        $data = $request->validated();
+
+        // generate slug dari nama vendor
+        $data['slug'] = Str::slug($data['name'], '-');
+
+        // simpan data vendor ke database
+        User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt('p@ssword'),
+            'role' => 'vendor',
+            'slug' => $data['slug'],
+            'phone' => $data['phone'],
+        ]);
+
+        return redirect()->route('vendor.dashboard')->with('success', 'Vendor Berhasil dibuat.');
     }
 }
