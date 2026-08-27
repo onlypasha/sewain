72c8c349bb48464e57c0bf680bab6f3ad37db7e7
onlypasha <onlypasha7@outlook.co.id>
2026-08-27 10:37:54 +0700

    adding subs controller

--- app/Http/Controllers/Superadmin/SubscriptionController.php
+++ app/Http/Controllers/Superadmin/SubscriptionController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers\Superadmin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Subscription;
 
 class SubscriptionController extends Controller
 {
     public function index()
     {
-        return view('superadmin.subscription');
+        $subscriptions = Subscription::latest()->get();
+        return view('superadmin.subscription', compact('subscriptions'));
     }
 }
